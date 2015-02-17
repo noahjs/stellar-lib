4237326099594f4c190c73ad66c8b0652e6a5bf7
Noah S <noah@noahjs.com>
2015-02-16 21:05:32 -0700

    Use sodium library

--- src/js/ripple/keypair.js
+++ src/js/ripple/keypair.js
@@ -1,10 +1,18 @@
 var sjcl = require('./utils').sjcl;
-var tnacl = require('tweetnacl');
 
 var UInt160 = require('./uint160').UInt160;
 var UInt256 = require('./uint256').UInt256;
 var Base    = require('./base').Base;
 var Crypt   = require('./crypt.js').Crypt;
+
+try {
+  var sodium = require('sodium');
+  var USE_SODIUM = true;
+} catch(e) {
+  var tnacl = require('tweetnacl');
+  var USE_SODIUM = false;
+}
+
 
 /**
  * Creates an ED25519 key pair for signing.
@@ -16,13 +24,35 @@ function KeyPair() {
 
 }
 
-KeyPair.from_seed_bytes = function(bytes) {
-  var keys = tnacl.sign.keyPair.fromSeed(new Uint8Array(bytes));
+KeyPair.from_seed_bytes_NACL = function(seed_bytes) {
+  var keys = tnacl.sign.keyPair.fromSeed(new Uint8Array(seed_bytes));
 
   var result = new KeyPair();
   result._secret = keys.secretKey;
   result._pubkey = keys.publicKey;
   return result;
+}
+
+KeyPair.from_seed_bytes_SODIUM = function(seed_bytes){
+  var seed_bits  = sjcl.codec.bytes.toBits(seed_bytes);
+  var seed_hex = sjcl.codec.hex.fromBits(seed_bits);
+
+  var keys = new sodium.Key.Sign.fromSeed( seed_hex, 'hex');
+  keys.setEncoding('hex');
+
+  var result = new KeyPair();
+  result._secret = hex_to_Uint8Array( keys.secretKey.toString('hex') );
+  result._pubkey = hex_to_Uint8Array( keys.publicKey.toString('hex') );
+
+  return result;
+}
+
+KeyPair.from_seed_bytes = function(bytes) {
+  if( USE_SODIUM ){
+    return KeyPair.from_seed_bytes_SODIUM(bytes);
+  }else{
+    return KeyPair.from_seed_bytes_NACL(bytes);
+  }
 }
 
 /**
@@ -81,11 +111,55 @@ KeyPair.prototype.get_address = function() {
   return address;
 };
 
-KeyPair.prototype.sign = function(hash) {
-  hash = UInt256.from_json(hash);
-  var sig = tnacl.sign(new Uint8Array(hash.to_bytes()), this._secret);
+KeyPair.sign_NACL = function(hash, secret) {
+  var hash_hex = UInt256.from_json(hash).to_hex();
 
-  return sjcl.codec.bytes.toBits(sig);
+  var sig = tnacl.sign(hex_to_Uint8Array(hash_hex), secret);
+  var sig_hex = new Buffer(sig).toString('hex');
+
+  var signed_bits = sjcl.codec.hex.toBits(sig_hex);
+  return signed_bits;
+}
+
+KeyPair.sign_SODIUM = function(hash, secret) {
+  var hash_hex = UInt256.from_json(hash).to_hex();
+
+  // Secret
+  var sec_hex   = Uint8Array_to_hex( secret ).slice(0, 64)
+
+  var key = new sodium.Key.Sign.fromSeed(sec_hex, 'hex');
+  var signer = new sodium.Sign(key);
+  var sig = signer.sign(hash_hex, 'hex');
+  var sig_hex = sig.sign.slice(0, 64).toString('hex');
+
+  var signed_bits = sjcl.codec.hex.toBits(sig_hex);
+  return signed_bits;
+}
+
+KeyPair.prototype.sign = function(hash) {
+  if( USE_SODIUM ){
+    return KeyPair.sign_SODIUM(hash, this._secret);
+  }else{
+    return KeyPair.sign_NACL(hash, this._secret);
+  }
+};
+
+
+var Uint8Array_to_hex = function(arr) {
+  var s = '';
+  for (var i = 0; i < arr.length; i++) {
+    s += arr[i].toString(16);
+  }
+  return s;
+};
+
+var hex_to_Uint8Array = function(input) {
+  var raw = new Buffer(input, 'hex');
+  var arr = new Uint8Array(new ArrayBuffer(raw.length));
+  for(i = 0; i < raw.length; i++) {
+    arr[i] = raw[i];
+  }
+  return arr;
 };
 
 exports.KeyPair = KeyPair;
